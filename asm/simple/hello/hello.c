9cbf5c1fa2451010b0bebb77c70bd4b09c8e5ba3
Kosik-Chris <kosik.chris.j@gmail.com>
2020-07-27 21:57:33 -0400

    asm

---
+++ asm/simple/hello/hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+    Comparing a super simple C program, using glibc vs. a asm64 
+    for printing.
+*/
+
+int main(void){
+    printf("Hello, world!");
+    exit(0);
+}
